9afdcc423159e06f3e6a0c4af313f1f9c9aaf342
Malte Lunkeit <malte.lunkeit@gmail.com>
2026-04-11 14:24:05 +0200

    Added toml configuration

--- cmd/blueice/main.go
+++ cmd/blueice/main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"BlueIce/internal/config"
 	"BlueIce/internal/server"
 	"log"
+	"os"
+	"path/filepath"
 )
 
 func main() {
 	log.Println("Starting server")
+
+	ex, _ := os.Executable()
+	path := filepath.Dir(ex)
+
+	serverConfig, _ := config.InitializeServerConfig(path)
+
+	log.Println(serverConfig)
 
 	minecraftServer := server.NewMinecraftServer(25565)
 	err := minecraftServer.Start()
--- internal/config/serverconfig.go
+++ internal/config/serverconfig.go
@@ -1,1 +1,40 @@
 package config
+
+import (
+	_ "embed"
+	"log"
+	"os"
+
+	"github.com/BurntSushi/toml"
+)
+
+type ServerConfig struct {
+	Server struct {
+		Port       uint16 `toml:"port"`
+		MaxPlayers uint16 `toml:"max_players"`
+		MOTD       string `toml:"motd"`
+	} `toml:"server"`
+}
+
+//go:embed default_config.toml
+var defaultConfigFile []byte
+
+func InitializeServerConfig(path string) (ServerConfig, error) {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		log.Println("Config file does not exist, exporting default config...")
+
+		err := os.WriteFile(path, defaultConfigFile, 0644)
+		if err != nil {
+			log.Fatal("Could not create default config", err)
+		}
+	} else if err != nil {
+		log.Fatal("Could not open config file", err)
+	}
+
+	var config ServerConfig
+	if _, err := toml.DecodeFile(path, &config); err != nil {
+		log.Fatal("Could not decode config file", err)
+	}
+
+	return config, nil
+}
